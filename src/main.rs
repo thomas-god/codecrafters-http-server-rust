45a2a4faed761d18bb193a366b641950a236e418
thomas-god <thomas.godlewski@icloud.com>
2025-01-30 19:46:21 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -11,7 +12,8 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
