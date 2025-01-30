8411f98ba25e7c00703dc0e3204f786facd4aff9
thomas-god <thomas.godlewski@icloud.com>
2025-01-30 20:34:34 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::io::Write;
+use std::io::{Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
+
+use request::Request;
+
+mod request;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,11 +13,20 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+                let mut buf = [0u8; 4096];
+                let n = stream.read(&mut buf).unwrap();
+                let Some(request) = Request::from_buffer(&buf[..n]) else {
+                    break;
+                };
+                match request.target.as_str() {
+                    "/" => stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap(),
+                    _ => stream.write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes()).unwrap()
+                };
+
                 println!("accepted new connection");
             }
             Err(e) => {
---
+++ src/request.rs
@@ -0,0 +1,52 @@
+
+#[derive(Debug, PartialEq)]
+pub enum Verb {
+    GET
+}
+
+impl Verb {
+    pub fn from_str(word: &str) -> Option<Verb> {
+        match word {
+            "GET" => Some(Verb::GET),
+            _ => None
+        }
+    }
+}
+pub struct Request {
+    pub verb: Verb,
+    pub target: String,
+}
+
+impl Request {
+    pub fn from_buffer(buffer: &[u8]) -> Option<Request> {
+        let content = String::from_utf8(buffer.to_vec()).ok()?;
+
+        let mut iter = content.split("\r\n");
+
+        let mut request_line = iter.next().map(|line| line.split(" "))?;
+
+        let verb = request_line.next().and_then(Verb::from_str)?;
+        let target = request_line.next()?;
+
+        Some(Request { verb, target: target.to_string() })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::request::Verb;
+
+    use super::Request;
+
+    #[test]
+    fn test_parse_request() {
+        let buffer = "GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n".as_bytes();
+        
+        let request = Request::from_buffer(&buffer);
+        assert!(request.is_some());
+
+        let request = request.unwrap();
+        assert_eq!(request.verb, Verb::GET);
+        assert_eq!(request.target, "/index.html".to_string());
+    }
+}
