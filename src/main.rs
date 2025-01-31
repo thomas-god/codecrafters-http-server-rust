abebf7050a5a813d4e2b5f718192e978cf3f35fc
thomas-god <thomas.godlewski@icloud.com>
2025-01-31 14:28:38 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, env, fs, path::PathBuf};
+
 use request::{Request, Verb};
 use response::{Response, Status};
 use tokio::{
@@ -12,20 +14,20 @@ mod response;
 async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+    let args = parse_args(env::args().collect());
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
 
     loop {
         let (stream, _) = listener.accept().await.unwrap();
+        let file_folder = args.get("directory").map(|s| s.to_owned());
         tokio::spawn(async move {
-            process_stream(stream).await;
+            process_stream(stream, file_folder).await;
         });
     }
 }
 
-async fn process_stream(mut stream: TcpStream) {
+async fn process_stream(mut stream: TcpStream, file_folder: Option<String>) {
     let mut buf = [0u8; 4096];
     match stream.read(&mut buf).await {
         Ok(n) => {
@@ -34,35 +36,49 @@ async fn process_stream(mut stream: TcpStream) {
             };
             match (request.verb, request.target.as_str()) {
                 (Verb::GET, "/") => stream
-                    .write_all(Response::new(Status::OK).as_string().as_bytes())
+                    .write_all(&Response::new(Status::OK).as_bytes())
                     .await
                     .unwrap(),
                 (Verb::GET, target) if target.starts_with("/echo/") => {
                     let echo_back = request.target.strip_prefix("/echo/").unwrap();
                     let mut response = Response::new(Status::OK);
-                    response.set_body(response::ContentType::Text, echo_back.to_owned());
-                    stream
-                        .write_all(response.as_string().as_bytes())
-                        .await
-                        .unwrap();
+                    response.set_body(response::Content::Text(echo_back.to_owned()));
+                    stream.write_all(&response.as_bytes()).await.unwrap();
                 }
                 (Verb::GET, "/user-agent") => {
                     let Some(user_agent) = request.headers.get("User-Agent") else {
                         stream
-                            .write_all(Response::new(Status::NotFound).as_string().as_bytes())
+                            .write_all(&Response::new(Status::NotFound).as_bytes())
                             .await
                             .unwrap();
                         return;
                     };
                     let mut response = Response::new(Status::OK);
-                    response.set_body(response::ContentType::Text, user_agent.to_owned());
-                    stream
-                        .write_all(response.as_string().as_bytes())
-                        .await
+                    response.set_body(response::Content::Text(user_agent.to_owned()));
+                    stream.write_all(&response.as_bytes()).await.unwrap();
+                }
+                (Verb::GET, target) if target.starts_with("/files/") => {
+                    let Some(folder) = file_folder else {
+                        return;
+                    };
+                    let file = target
+                        .strip_prefix("/files/")
+                        .map(|s| s.to_owned())
                         .unwrap();
+                    let file_path = [folder, file].iter().collect::<PathBuf>();
+                    let Ok(file) = fs::read(file_path) else {
+                        stream
+                            .write_all(&Response::new(Status::NotFound).as_bytes())
+                            .await
+                            .unwrap();
+                        return;
+                    };
+                    let mut response = Response::new(Status::OK);
+                    response.set_body(response::Content::OctetStream(file));
+                    stream.write_all(&response.as_bytes()).await.unwrap();
                 }
                 _ => stream
-                    .write_all(Response::new(Status::NotFound).as_string().as_bytes())
+                    .write_all(&Response::new(Status::NotFound).as_bytes())
                     .await
                     .unwrap(),
             };
@@ -73,4 +89,27 @@ async fn process_stream(mut stream: TcpStream) {
             println!("error: {}", e);
         }
     }
+}
+
+fn parse_args(args_list: Vec<String>) -> HashMap<String, String> {
+    let mut args = HashMap::<String, String>::new();
+
+    let mut args_iter = args_list.into_iter();
+
+    // Pop first element (program path)
+    let _ = args_iter.next();
+
+    // Iterate over pairs of (--option, value)
+    while let Some(option) = args_iter.next() {
+        let Some(option) = option.strip_prefix("--").map(|s| s.to_string()) else {
+            break;
+        };
+        let Some(value) = args_iter.next() else {
+            println!("No value found for option {option}, passing...");
+            break;
+        };
+        args.insert(option, value);
+    }
+
+    args
 }
--- src/response.rs
+++ src/response.rs
@@ -13,69 +13,86 @@ impl Status {
     }
 }
 
-pub enum ContentType {
-    Text,
+pub enum Content {
+    Text(String),
+    OctetStream(Vec<u8>),
 }
 
 pub struct Response {
     pub status: Status,
-    pub content_type: Option<ContentType>,
-    pub body: Option<String>,
+    pub body: Option<Content>,
 }
 
 impl Response {
     pub fn new(status: Status) -> Response {
-        Response {
-            status,
-            content_type: None,
-            body: None,
-        }
+        Response { status, body: None }
     }
 
-    pub fn set_body(&mut self, content_type: ContentType, body: String) {
-        self.content_type = Some(content_type);
+    pub fn set_body(&mut self, body: Content) {
         self.body = Some(body);
     }
 
-    pub fn as_string(&self) -> String {
+    pub fn as_bytes(&self) -> Vec<u8> {
         let status_line = format!(
-            "HTTP/1.1 {} {}",
+            "HTTP/1.1 {} {}\r\n",
             self.status as u16,
             self.status.as_string()
         );
         let mut headers = String::new();
-        let mut body = String::new();
-        if let (Some(_content_type), Some(_body)) = (&self.content_type, &self.body) {
-            headers.push_str("Content-Type: text/plain\r\n");
-            headers.push_str(&format!("Content-Length: {}\r\n", _body.as_bytes().len()));
-            body = _body.to_owned();
-        }
+        let body = match &self.body {
+            Some(Content::Text(content)) => {
+                headers.push_str("Content-Type: text/plain\r\n");
+                headers.push_str(&format!("Content-Length: {}\r\n", content.as_bytes().len()));
+                content.as_bytes()
+            }
+            Some(Content::OctetStream(bytes)) => {
+                headers.push_str("Content-Type: application/octet-stream\r\n");
+                headers.push_str(&format!("Content-Length: {}\r\n", bytes.len()));
+                bytes
+            }
+            _ => &[],
+        };
+        headers.push_str("\r\n");
 
-        [status_line, headers, body].join("\r\n")
+        let mut bytes = Vec::<u8>::new();
+        bytes.extend_from_slice(status_line.as_bytes());
+        bytes.extend_from_slice(headers.as_bytes());
+        bytes.extend_from_slice(body);
+        bytes
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::response::{ContentType, Response, Status};
+    use crate::response::{Content, Response, Status};
 
     #[test]
     fn test_response_as_string() {
         assert_eq!(
-            Response::new(Status::OK).as_string(),
-            "HTTP/1.1 200 OK\r\n\r\n".to_owned()
+            Response::new(Status::OK).as_bytes(),
+            "HTTP/1.1 200 OK\r\n\r\n".to_owned().as_bytes()
         );
         assert_eq!(
-            Response::new(Status::NotFound).as_string(),
-            "HTTP/1.1 404 Not Found\r\n\r\n".to_owned()
+            Response::new(Status::NotFound).as_bytes(),
+            "HTTP/1.1 404 Not Found\r\n\r\n".to_owned().as_bytes()
         );
 
         let mut res = Response::new(Status::OK);
-        res.set_body(ContentType::Text, "toto".to_owned());
+        res.set_body(Content::Text("toto".to_owned()));
         assert_eq!(
-            res.as_string(),
+            res.as_bytes(),
             "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 4\r\n\r\ntoto"
                 .to_owned()
+                .as_bytes()
         );
+
+        let mut res = Response::new(Status::OK);
+        res.set_body(Content::OctetStream("toto".as_bytes().to_vec()));
+        assert_eq!(
+            res.as_bytes(),
+            "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: 4\r\n\r\ntoto"
+                .to_owned()
+                .as_bytes()
+        )
     }
 }
